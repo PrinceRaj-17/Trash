c077f5d2c74384dc0c59daf67e21285ca54b6678
Prince Raj <princerajbhar26@gmail.com>
2021-05-06 10:53:35 +0530

    added signout

---
+++ main.js
@@ -0,0 +1,25 @@
+
+    var firebase = app_fireBase;
+    var uid = null;
+    firebase.auth().languageCode = 'it';
+    // To apply the default browser preference instead of explicitly setting it.
+    // firebase.auth().useDeviceLanguage();
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          uid = user.uid;
+        }else{
+            uid = null;
+            window.location.replace("login.html");
+        }
+      });
+
+      function logOut(){
+        firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+            window.location.replace("index.html");
+          }).catch((error) => {
+            // An error happened.
+          });
+          
+      }
